414b7156310d0c0a8b4fb5a66d54a0ab223e4bf1
ahazen94 <alexhazen1@gmail.com>
2024-03-05 15:05:39 -0800

    Creating Assignment 3

--- script/temperature.js
+++ script/temperature.js
@@ -1,4 +1,13 @@
-function convert(){
-    let temperature=Number(prompt("Enter your temperature in Celsius"));
-    document.getElementById("results").innerHTML=`The conversion of Celsius ${temperature} to Farenheit is ${farenheit=((temperature*9/5)+32)}`;
+function convertTemperature(){
+    let temperature=Number(prompt("Enter your temperature (Either Farenheit or Celsius"));
+    let operation= prompt(
+        `Choose Either Option
+        1-Farenheit
+        2-Celsius`
+    )
+    if(operation== 1){
+        document.getElementById("results").innerHTML=`The conversion of Farenheit ${temperature} to Celsius is ${celsius=(temperature*9/5)}`;
+    }if(operation== 2){
+        document.getElementById("results").innerHTML=`The conversion of Celsius ${temperature} to Farenheit is ${farenheit=((temperature*9/5)+32)}`;
+    }
 }
